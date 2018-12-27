9359733d8a0c73997150d02a207dea38b53c5669
DanFoad <daniel.foad@sky.uk>
2018-12-27 00:29:25 +0000

    Added existing labels, hardcoded for now

--- danfoad/src/main/scala/com/danfoad/LabelRegistryActor.scala
+++ danfoad/src/main/scala/com/danfoad/LabelRegistryActor.scala
@@ -14,7 +14,16 @@ object LabelRegistryActor {
 class LabelRegistryActor extends Actor with ActorLogging {
   import LabelRegistryActor._
 
-  var labels = Map("general.name" -> "Dan Foad")
+  var labels = Map(
+    "general.name" -> "Dan Foad",
+    "general.role" -> "Associate Software Developer",
+    "home.header.subline" -> "Currently working with NowTV",
+    "home.header.contact" -> "Contact Me",
+    "app.nav.links.home" -> "Home",
+    "app.nav.links.projects" -> "Projects",
+    "app.nav.links.cv" -> "CV",
+    "app.nav.links.blog" -> "Blog",
+    "app.nav.links.contact" -> "Contact Me")
 
   def receive: Receive = {
     case GetLabels =>
